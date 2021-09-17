5f0426fbeb8b98094e0ec6f5e121a0e03d7d977c
veteranus666 <90448566+veteranus666@users.noreply.github.com>
2021-09-17 11:22:48 +0300

    Update Source.cpp

--- opencv/Source.cpp
+++ opencv/Source.cpp
@@ -1,4 +1,3 @@
-
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
@@ -13,7 +12,7 @@ using namespace std;
 int main(int argc, char** argv)
 {
 
-	int height = 520;
+	/*int height = 520;
 	int width = 840;
 	Mat img(height, width, CV_8UC3);
 
@@ -22,10 +21,17 @@ int main(int argc, char** argv)
 	double fontScale = 2;
 	Scalar color(200, 100, 50);
 	putText(img, "OpenCV step By Step", textOrg, fontFace, fontScale, color);
-
-	imshow("Hello World", img);
-
-	Mat imgt = imread("read.jpg", 1);
+	namedWindow("helloy",0);
+	imshow("helloy", img);
+	waitKey(0);*/
+		setlocale(LC_ALL, "Russian");
+	char filename[80]; // read.jpg
+		cout << "Введите имя файла, в который хотите внести изменения, и нажмите Enter" << endl;
+	cin.getline(filename, 80);
+	cout << "Открыть файл";
+	cout << filename << endl;
+	Mat imgt = imread(filename, 1);
+	
 	imshow("Load", imgt);
 	waitKey(0);
 	system("pause");

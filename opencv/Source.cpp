aef4191b1282af1e317eb7c3185c99e415cb31db
veteranus666 <90448566+veteranus666@users.noreply.github.com>
2021-09-17 12:04:45 +0300

    Update Source.cpp

--- opencv/Source.cpp
+++ opencv/Source.cpp
@@ -1,3 +1,4 @@
+
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
@@ -30,9 +31,21 @@ int main(int argc, char** argv)
 	cin.getline(filename, 80);
 	cout << "Открыть файл";
 	cout << filename << endl;
-	Mat imgt = imread(filename, 1);
+	Mat img = imread(filename, 1);
 	
-	imshow("Load", imgt);
+	imshow("Load", img);
+	Mat scr_gray;
+	Mat canny_output;
+	Mat _img;
+	cvtColor(img, scr_gray, COLOR_RGB2GRAY);
+	blur(scr_gray, scr_gray, Size(3, 3));
+
+	double otsu_thresh_val = threshold(scr_gray, _img, 0, 255, THRESH_BINARY | THRESH_OTSU);
+	double hight_thresh_val = otsu_thresh_val, lower_thresh_val = otsu_thresh_val * 0.5;
+		cout << otsu_thresh_val;
+	Canny(scr_gray, canny_output, lower_thresh_val, hight_thresh_val, 3);
+	imshow("Load", canny_output);
+	imwrite("canny_output.jpg", canny_output);
 	waitKey(0);
 	system("pause");
 
